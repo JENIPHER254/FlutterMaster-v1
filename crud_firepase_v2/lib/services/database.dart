be70a711a45be65b897c24351b311fbe3040f18b
JENIPHER254 <jeniperkuki@gmail.com>
2024-03-21 09:26:06 +0300

    db connection

--- crud_firepase_v2/lib/services/database.dart
+++ crud_firepase_v2/lib/services/database.dart
@@ -1,1 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+class DatabaseMethod {
+  Future addEmployeeDetails(
+      Map<String, dynamic> employeeinfoMap, String id) async {
+    return await FirebaseFirestore.instance
+        .collection("Employee")
+        .doc(id)
+        .set(employeeinfoMap);
+  }
+}
