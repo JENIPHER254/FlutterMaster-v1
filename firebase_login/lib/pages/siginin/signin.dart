dd6a9628b3fbac610c9009dd2b0a480c1220ded9
JENIPHER <jeniperkuki@gmail.com>
2024-04-02 17:37:38 +0300

    siginin ui

--- firebase_login/lib/pages/siginin/signin.dart
+++ firebase_login/lib/pages/siginin/signin.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_login/widgets/big_text.dart';
+import 'package:firebase_login/widgets/loginreg_button.dart';
 import 'package:flutter/material.dart';
 
 class Signin extends StatefulWidget {
@@ -10,6 +12,75 @@ class Signin extends StatefulWidget {
 class _SigninState extends State<Signin> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    //String userEmail, userPassword;
+    // TextEditingController _emailController = TextEditingController();
+    // TextEditingController _passwordController = TextEditingController();
+
+    getUserEmail(email) {
+      return print(email);
+    }
+
+    getUserPassword(password) {
+      return print(password);
+    }
+
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              child: Image(
+                image: AssetImage("assets/images/wave1.jpeg"),
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+            Center(
+                child: big_text(
+                    "Welcome Back... ", Color.fromARGB(255, 108, 18, 124))),
+            SizedBox(
+              height: 15,
+            ),
+            big_text("Signin", Colors.black),
+            SizedBox(
+              height: 35,
+            ),
+            SingleChildScrollView(
+              child: Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: TextField(
+                      //controller: _emailController,
+                      onChanged: (String email) {
+                        getUserEmail(email);
+                      },
+                      decoration:
+                          InputDecoration(label: Text("Email / Username")),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: TextField(
+                      //controller: _passwordController,
+                      onChanged: (String password) {
+                        getUserPassword(password);
+                      },
+                      decoration: InputDecoration(label: Text("Password")),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  loginreg_button("Signin", () {
+                    Navigator.pushNamed(context, "home");
+                  })
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
