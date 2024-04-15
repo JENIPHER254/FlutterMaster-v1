1042b86230ed9731029c01874c6d689d3e55c760
JENIPHER <jeniperkuki@gmail.com>
2024-04-15 20:36:32 +0300

    debug

--- todo_app/lib/pages/login/login.dart
+++ todo_app/lib/pages/login/login.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:todo_app/widgets/bg_text/bg_text.dart';
+
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -10,6 +13,18 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            bgText("Sign In", Color.fromARGB(255, 114, 113, 113)),
+          ],
+        ),
+      ),
+    );
   }
 }
