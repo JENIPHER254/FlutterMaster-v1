627bb547430a387238394be98f6181b92ebf3b1e
JENIPHER254 <jeniperkuki@gmail.com>
2025-08-08 11:52:38 +0300

    feat: pageview automatic handscroll-e.g tiktok video owner profile view effect

--- flutter_widgets/lib/page_view_wiget/horizontal_auto.dart
+++ flutter_widgets/lib/page_view_wiget/horizontal_auto.dart
@@ -6,13 +6,63 @@ class HorizontalAutomatic extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      child: Center(
-        child: Text(
-          'This is the PageView Demo page.',
-          style: TextStyle(fontSize: 24),
-        ),
+      body: PageView(
+        scrollDirection: Axis.horizontal,
+        children: [
+          Container(
+            color: Colors.red,
+            child: Center(
+                child: Text('Page 1',
+                    style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold))),
+          ),
+          Container(
+            color: Colors.green,
+            child: Center(
+                child: Text('Page 2',
+                    style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold))),
+          ),
+          Container(
+            color: Colors.blue,
+            child: Center(
+                child: Text('Page 3',
+                    style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold))),
+          ),
+          Container(
+            color: Colors.orange,
+            child: Center(
+                child: Text('Page 4',
+                    style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold))),
+          ),
+          Container(
+            color: Colors.purple,
+            child: Center(
+                child: Text('Page 5',
+                    style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold))),
+          ),
+        ],
       ),
-    ));
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pushNamed(context, '/');
+        },
+        child: Icon(Icons.home, color: Colors.red),
+        backgroundColor: Colors.white,
+      ),
+    );
   }
 }
